c9209a6a82acea3f936aefde2357462f27a02c92
thiago-caldas-pires <thiago.pires@gmail.com>
2020-08-20 09:59:41 -0300

    create branch

---
+++ app/src/main/java/com/relatoriodecelula/CellReportAdapter.kt
@@ -0,0 +1,27 @@
+package com.relatoriodecelula
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CellReportAdapter(
+    private val cellList: ArrayList<CelulaBO>
+) : RecyclerView.Adapter<CellHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CellHolder {
+        return CellHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.custom_cell_item, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return cellList.size
+    }
+
+    override fun onBindViewHolder(holder: CellHolder, position: Int) {
+        holder.detail.text = cellList[position].week.toString()
+        holder.members.text = cellList[position].members
+        holder.visitors.text = cellList[position].visitors
+        holder.regulars.text = cellList[position].regulars
+    }
+}
